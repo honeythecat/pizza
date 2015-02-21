eb67595e1600a1319bf7eb802c42108533b65110
Heather <heather@thisisnotanorg.org>
2015-02-20 16:51:33 -0800

    Add last commit before 5:00.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 README
 
-##Pizza!
+##CAPS LOCK PIZZA PARLOUR!
 by Heather Anderson
 
-A simple js app that lets a user ------------
+A simple js app that lets a user enter in a pizza type and size to find out the number of pieces it will be cut into. Nothing fancy, nothing random. I promise. Would I lie to you?
 
 ##Setup
 
@@ -17,7 +17,7 @@ Enjoy!
 Testing the js methods can be done via spec-runner.html.
 
 ##Known Bugs as of 2/20/15
----------
+Not exactly a bug, but I would like the bottom part to be hidden until you click it. Silly css.
 
 ##Motivation
 
@@ -27,4 +27,4 @@ To practice writing JS apps and specs.
 
 Heather Anderson. MIT License. Copyright 2015. Created by Heather Anderson. Email: heather@thisisnotanorg.org.
 
-Note to self: -----------------
+Note to self: IT'S FRIDAY! YOU MADE AN APP! IT WORKS! Remember to eat a food tonight, because your brain is a bit fuzzy and you feel a bit ill. Food is required for coding, especially when coding with the word pizza for 8 hours.
--- js/scripts.js
+++ js/scripts.js
@@ -9,43 +9,28 @@ var Pizza = {
 };
 
 
-
 // ------------------------------------------
 
 $(document).ready(function() {
   $("form#new-pizza").submit(function(event) {
     event.preventDefault();
 
-
     var inputtedFlavor = $("input#pizza-type").val();
     var inputtedSize = $("input#pizza-size").val();
     var newPizza = { size: inputtedSize, flavor: inputtedFlavor };
 
-
     $(".pizza-type").text(newPizza.flavor);
 
+    if (newPizza.flavor == "CHEESE") {
+      unroundedCheeseSlices = newPizza.size / 2;
+      numberOfCheeseSlices = 2 * Math.round(unroundedCheeseSlices/2);
+      numberOfSlices = numberOfCheeseSlices;
+    } else if (newPizza.flavor == "PEPPERONI") {
+      numberOfSlices = Math.floor(Math.random() * 100) || 0;
+    } else {
+      alert ("WE ONLY MAKE PEPPERONI AND CHEESE AND ONLY IF IT'S TYPED IN CAPS LOCK! YOU GET NO PIZZA!!!!");
+    };
 
-
-  if (newPizza.flavor == "CHEESE") {
-    unroundedCheeseSlices = newPizza.size / 2;
-    numberOfCheeseSlices = 2 * Math.round(unroundedCheeseSlices/2);
-    numberOfSlices = numberOfCheeseSlices;
-  } else if (newPizza.flavor == "PEPPERONI") {
-    numberOfSlices = Math.floor(Math.random() * 100) || 0;
-  } else {
-    alert ("WE ONLY MAKE PEPPERONI AND CHEESE AND ONLY IF IT'S TYPED IN CAPS LOCK! YOU GET NO PIZZA!!!!");
-  };
-
-  $(".slice-number").text(numberOfSlices);
-
-
-
-
-
-
-
-
-debugger;
-    });
-
+    $(".slice-number").text(numberOfSlices);
   });
+});
